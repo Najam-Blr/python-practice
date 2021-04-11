f4f7cf54c0592fbb362597e96a05c0d5e6bc729c
Najamunnisa Begum <64567091+Najam-Blr@users.noreply.github.com>
2021-04-10 21:34:00 -0400

    Update pythonEnum.py

--- pythonEnum.py
+++ pythonEnum.py
@@ -1,1 +1,25 @@
+from enum import Enum, IntEnum
+
+class Weekdays(IntEnum):
+    Saturday = 6
+    Monday = 1
+    Tuesday = 2
+    Friday = 5
+    Wednesday = 3
+    Thursday = 4
+    Weekend = 5 #duplicate values are not considered 
+
+print("The week day is = {} and it's numerical value = {}".format(Weekdays.Monday.name, Weekdays.Monday.value))
+
+for weekday in Weekdays:
+    print(weekday.name, weekday.value)
+
+print("Sorted the ENum")
+for c in sorted(Weekdays):
+    print(c.name, c.value)
+
+#using map func to iterate the enum class
+weekday_list = list(map(int, Weekdays))
+print(weekday_list)
+
 
