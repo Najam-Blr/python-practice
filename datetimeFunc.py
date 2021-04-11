64a7d28fdf57931f33f066e362cc071d1a785d18
Najamunnisa Begum <64567091+Najam-Blr@users.noreply.github.com>
2021-04-10 21:55:38 -0400

    Update datetimeFunc.py

--- datetimeFunc.py
+++ datetimeFunc.py
@@ -1,5 +1,6 @@
 import datetime
 print("Current date and time: " , datetime.datetime.now())
+print"Current time ", (datetime.datetime.now().time())
 print("Current year: ", datetime.date.today().strftime("%Y"))
 print("Month of year: ", datetime.date.today().strftime("%B"))
 print("Week number of the year: ", datetime.date.today().strftime("%W"))
@@ -7,3 +8,4 @@ print("Weekday of the week: ", datetime.date.today().strftime("%w"))
 print("Day of year: ", datetime.date.today().strftime("%j"))
 print("Day of the month : ", datetime.date.today().strftime("%d"))
 print("Day of week: ", datetime.date.today().strftime("%A"))
+print("5 days before current date", datetime.date.today()-datetime.timedelta(5))
